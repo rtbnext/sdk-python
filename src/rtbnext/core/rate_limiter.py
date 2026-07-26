299e6931dd6a3ef61ab14422fc4880ddf2c474d8
komed3 <webmaster@komed3.de>
2026-07-27 01:36:30 +0200

    process waiting requests while tokens are available

--- src/rtbnext/core/rate_limiter.py
+++ src/rtbnext/core/rate_limiter.py
@@ -30,3 +30,14 @@ class RateLimiter:
 
     self._queue: deque[ asyncio.Future[ None ] ] = deque()
     self._task: asyncio.Task[ None ] | None = None
+
+  def _process_queue ( self ) -> None:
+    """Process waiting requests while tokens are available."""
+
+    while self._tokens > 0 and self._queue:
+      self._tokens -= 1
+
+      future = self._queue.popleft()
+
+      if not future.done():
+        future.set_result( None )
