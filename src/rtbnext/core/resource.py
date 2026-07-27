1d2cbc946804f7bdfb71de258ae427e626debf53
komed3 <webmaster@komed3.de>
2026-07-27 11:38:33 +0200

    fix imports

--- src/rtbnext/core/resource.py
+++ src/rtbnext/core/resource.py
@@ -6,12 +6,15 @@ Implements the resource loader and pooling of resource states.
 
 
 from __future__ import annotations
+
 from dataclasses import dataclass
 from time import time
-from typing import Any, Callable, Generic, Literal, TypeVar, TYPE_CHECKING
+from typing import Any, Callable, Generic, Literal, TypeVar
+
 import httpx
-from rtbnext.core.http_client import HttpResponse, HttpClient, RequestOptions
+
 from rtbnext.core.cache import Cache, EmptyCache, MemoryCache
+from rtbnext.core.http_client import HttpClient, HttpResponse, RequestOptions
 from rtbnext.resource.resource import Resource
 
 
