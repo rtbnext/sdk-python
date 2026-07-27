849b409da424c0906e0c155fadb9563cdb22aa63
komed3 <webmaster@komed3.de>
2026-07-27 11:19:51 +0200

    declare cache options

--- src/rtbnext/core/resource.py
+++ src/rtbnext/core/resource.py
@@ -6,10 +6,26 @@ Implements the resource loader and pooling of resource states.
 
 
 from __future__ import annotations
-
 from dataclasses import dataclass
-
+from time import time
+from typing import Any, Callable, Generic, Literal, TypeVar, TYPE_CHECKING
+import httpx
 from rtbnext.core.http_client import HttpResponse
+from rtbnext.core.cache import Cache, EmptyCache, MemoryCache
+from rtbnext.resource.resource import Resource
+
+
+CacheMode = Literal[ "ttl", "session", "revalidate" ]
+
+
+@dataclass ( slots= True )
+class CacheOptions:
+    """
+    Configuration options for the resource loader cache.
+    """
+
+    type: Cache | Literal[ False, "memory" ] = "memory"
+    mode: CacheMode = "ttl"
 
 
 @dataclass ( slots= True )
