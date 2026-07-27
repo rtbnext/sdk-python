10e3f5391a1c96be63701a44b33c90447a6867ec
komed3 <webmaster@komed3.de>
2026-07-27 10:28:20 +0200

    declare ResourceState dataclass

---
+++ src/rtbnext/core/resource.py
@@ -0,0 +1,25 @@
+"""
+RESOURCE
+
+Implements the resource loader and pooling of resource states.
+"""
+
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from http_client import HttpResponse
+
+
+@dataclass ( slots= True )
+class ResourceState:
+    """
+    Represents the state of a cached resource.
+    """
+
+    response: HttpResponse
+    created: float
+    expires: float | None = None
+    etag: str | None = None
+    last_modified: str | None = None
