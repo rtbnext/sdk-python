a9c1491356514476860422a2b532d9caa7b804ed
komed3 <webmaster@komed3.de>
2026-07-28 11:57:47 +0200

    continuously refill tokens one by one for spread mode

--- src/rate_limiter.py
+++ src/rate_limiter.py
@@ -44,3 +44,17 @@ class RateLimiter:
         self._tokens = self._max_requests
         self._task = None
         self._process_queue()
+
+    async def _spread_refill ( self ) -> None:
+        """Continuously refill tokens one by one."""
+
+        while True:
+            await asyncio.sleep( self._refill_interval )
+
+            if self._tokens < self._max_requests:
+                self._tokens += 1
+                self._process_queue()
+
+            if self._tokens == self._max_requests and not self._queue:
+                self._task = None
+                return
