5c7e37e609960a70231368b949812c38c4d07744
komed3 <webmaster@komed3.de>
2026-07-29 16:21:48 +0200

    prepare collectable resource

--- src/rtbnext/resource/collectable.py
+++ src/rtbnext/resource/collectable.py
@@ -4,7 +4,7 @@ Collectable Resource
 Implements the resource wrapper for collectable endpoints.
 """
 
-from typing import TypedDict
+from typing import Callable, TypedDict, TypeVar
 
 
 class CollectItem( TypedDict ):
@@ -17,3 +17,10 @@ class CollectData[ T: CollectItem ]( TypedDict ):
     """Ensure `items` is a list of collect items."""
 
     items: list[ T ]
+
+K = TypeVar( "K" )
+T = TypeVar( "T", bound= dict )
+R = TypeVar( "R" )
+
+type FindFn[ T ] = Callable[ [ list[ T ], str ], T | None ]
+type SearchFn[ T ] = Callable[ [ T, str, list[ str ] ], bool ]
