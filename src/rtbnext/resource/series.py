58623c1dd821d4558879101a9f5659734611d984
komed3 <webmaster@komed3.de>
2026-07-29 23:59:42 +0200

    declare dicts

--- src/rtbnext/resource/series.py
+++ src/rtbnext/resource/series.py
@@ -10,3 +10,21 @@ from typing import TypedDict
 class TimePoint( TypedDict ):
     """Ensure `date` is included in the dict."""
     date: str
+
+
+class AggregateRange( TypedDict ):
+    """The date range covered by this aggregate point."""
+
+    from_: str
+    to: str
+
+
+class AggregateValue( TypedDict ):
+    """Aggregated numeric summary values."""
+
+    first: float
+    last: float
+    min: float
+    max: float
+    avg: float
+    sum: float
