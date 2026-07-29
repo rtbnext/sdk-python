c2471c4a6b0a91b14edb738c8948e9ffb0f8c0c8
komed3 <webmaster@komed3.de>
2026-07-29 23:19:25 +0200

    Create rtbnext.py

---
+++ src/rtbnext/rtbnext.py
@@ -0,0 +1,8 @@
+class RTBNext:
+    """
+    Main entry point of the RTBNext SDK.
+
+    Provides access to all RTBNext API endpoints through a single client
+    instance while internally managing HTTP communication, resource loading,
+    caching, and endpoint initialization.
+    """
