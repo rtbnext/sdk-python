4d9c9ea91660d712500c93b7ef10bb7c4f5ad489
komed3 <webmaster@komed3.de>
2026-07-29 12:11:54 +0200

    declare the DateableResource class

--- src/rtbnext/resource/dateable.py
+++ src/rtbnext/resource/dateable.py
@@ -4,9 +4,12 @@ Dateable Resource
 Implements the resource wrapper for date-indexed endpoints
 """
 
-from typing import Generic, TypedDict, TypeVar
+from typing import Generic, TypedDict, TypeVar, Callable
 
 from rtbnext.resource.collection import DateCollectionBase
+from rtbnext.resource.base import Resource
+from rtbnext.core.resource import ResourceLoader
+from rtbnext.core.parser import ParserFn
 
 
 class DateData( TypedDict ):
@@ -16,6 +19,7 @@ class DateData( TypedDict ):
 
 D = TypeVar( "D", bound= DateData )
 R = TypeVar( "R" )
+type DateFn[ R ] = Callable[ [ str ], R ]
 
 
 class DateCollection( DateCollectionBase[ R ], Generic[ R ] ):
@@ -27,3 +31,23 @@ class DateCollection( DateCollectionBase[ R ], Generic[ R ] ):
     based on their date keys, while maintaining the underlying collection of date
     strings.
     """
+
+
+class DateableResource( Resource[ D ], Generic[ D, R ] ):
+    """
+    Resource wrapper for date-indexed endpoints.
+
+    This class provides lazy traversal over API endpoints that are indexed by
+    date values. It allows for convenient access to resources based on their
+    date keys, while maintaining the underlying resource data.
+    """
+
+    def __init__(
+        self,
+        path: str,
+        loader: ResourceLoader,
+        parser: ParserFn[ D ],
+        date: DateFn[ R ]
+    ):
+        super().__init__( path, loader, parser )
+        self._factory = date
