9d4734f3c059d3454f9ecd10a510d88844b41ad5
komed3 <webmaster@komed3.de>
2026-07-30 19:54:58 +0200

    describe the shape of asset data

--- src/rtbnext/schema/assets.py
+++ src/rtbnext/schema/assets.py
@@ -7,7 +7,25 @@ and historical financial data.
 
 from typing import NotRequired, TypedDict
 
+from rtbnext.schema.generic import AssetType
+
 ChangeItem = TypedDict( "ChangeItem", {
     "value": float,
     "percent": float
+} )
+
+AssetInfo = TypedDict( "AssetInfo", {
+    "exchange": str,
+    "ticker": str,
+    "shares": NotRequired[ float ],
+    "price": float,
+    "currency": str,
+    "exRate": float
+} )
+
+Asset = TypedDict( "Asset", {
+    "type": AssetType,
+    "label": str,
+    "value": NotRequired[ float ],
+    "info": NotRequired[ AssetInfo ]
 } )
