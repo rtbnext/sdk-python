6def8ecd6c5f01db6d2d2e81b45a0e5a422d0ec4
komed3 <webmaster@komed3.de>
2026-07-30 19:16:43 +0200

    declare the profile search index

--- src/rtbnext/schema/profile.py
+++ src/rtbnext/schema/profile.py
@@ -7,7 +7,9 @@ information, financial data, relationships, media, and historical statistics.
 
 from typing import NotRequired, TypedDict
 
-from rtbnext.schema.generic import MetaData
+from rtbnext.schema.generic import (
+    Gender, Industry, MaritalStatus, MetaData, ProfileStatusFlag, SelfMadeRank
+)
 
 ProfileIndexItem = TypedDict( "ProfileIndexItem", {
     "uri": str,
@@ -22,4 +24,42 @@ ProfileIndex = TypedDict( "ProfileIndex", {
     "$metadata": MetaData,
     "count": int,
     "items": list[ ProfileIndexItem ]
+} )
+
+SearchIndexItem = TypedDict( "SearchIndexItem", {
+    "id": str,
+    "uri": str,
+    "deceased": bool,
+    "family": bool,
+    "dropOff": bool,
+    "embargo": bool,
+    "searchName": str,
+    "fullName": str,
+    "lastName": str,
+    "gender": NotRequired[ Gender ],
+    "birthDate": NotRequired[ str ],
+    "age": NotRequired[ int ],
+    "birthCountry": NotRequired[ str ],
+    "residenceCountry": NotRequired[ str ],
+    "citizenship": NotRequired[ str ],
+    "industry": NotRequired[ Industry ],
+    "source": NotRequired[ list[ str ] ],
+    "networth": NotRequired[ float ],
+    "rank": NotRequired[ int ],
+    "organization": NotRequired[ str ],
+    "maritalStatus": NotRequired[ MaritalStatus ],
+    "children": NotRequired[ int ],
+    "philanthropyScore": NotRequired[ int ],
+    "selfMade": NotRequired[ bool ],
+    "selfMadeRank": NotRequired[ SelfMadeRank ],
+    "wikidata": NotRequired[ str ],
+    "status": NotRequired[ ProfileStatusFlag ],
+    "score": NotRequired[ float ],
+    "timestamp": str
+} )
+
+SearchIndex = TypedDict( "SearchIndex", {
+    "$metadata": MetaData,
+    "count": int,
+    "items": list[ SearchIndexItem ]
 } )
