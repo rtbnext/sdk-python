3fead2ff85362638be7a3b57652e956e68b76687
komed3 <webmaster@komed3.de>
2026-07-30 12:32:02 +0200

    declare the main entry point method for the Python SDK

--- src/rtbnext/__init__.py
+++ src/rtbnext/__init__.py
@@ -1,6 +1,6 @@
 """
 RTBNext Python SDK
- 
+
 Official Python SDK for the RTBNext API.
 
 Provides access to all RTBNext API endpoints, resources, and data models.
@@ -12,4 +12,38 @@ Author: Paul Köhler (komed3)
 License: MIT
 """
 
+from rtbnext.core.http_client import ClientIdentity
+from rtbnext.core.resource import CacheMode, CacheType
+from rtbnext.defaults import (
+    DEFAULT_API_URL, DEFAULT_CACHE_MODE, DEFAULT_CACHE_TYPE, DEFAULT_TIMEOUT
+)
+from rtbnext.rtbnext import RTBNext
+
 __version__ = "1.0.0"
+
+
+def rtbnext(
+    client: ClientIdentity, *,
+    base_url: str = DEFAULT_API_URL,
+    timeout: float = DEFAULT_TIMEOUT,
+    cache: CacheType = DEFAULT_CACHE_TYPE,
+    mode: CacheMode = DEFAULT_CACHE_MODE
+) -> RTBNext:
+    """
+    Creates a new RTBNext SDK instance.
+
+    Args:
+        client: Client identity information.
+        base_url: RTBNext API base URL.
+        timeout: HTTP request timeout.
+        cache: Cache implementation.
+        mode: Cache mode.
+
+    Returns:
+        A configured RTBNext SDK instance.
+    """
+
+    return RTBNext(
+        client, base_url= base_url, timeout= timeout,
+        cache= cache, mode= mode
+    )
