5e61a46941ea5f1f42e11cff765e8af6740d7d68
komed3 <webmaster@komed3.de>
2026-07-30 10:04:08 +0200

    manage imports

--- src/rtbnext/endpoint/base.py
+++ src/rtbnext/endpoint/base.py
@@ -4,6 +4,18 @@ Base Endpoint
 Implements the base endpoint class.
 """
 
+from __future__ import annotations
+
+from typing import Any
+
+from rtbnext.core.parser import Parser
+from rtbnext.core.resource import ResourceLoader, ResourcePool
+from rtbnext.resource.base import Resource
+from rtbnext.resource.collectable import CollectableResource
+from rtbnext.resource.dateable import DateableResource
+from rtbnext.resource.time_series import TimeSeriesResource
+
+
 class EndpointBase:
     """
     Base class for all API endpoints.
