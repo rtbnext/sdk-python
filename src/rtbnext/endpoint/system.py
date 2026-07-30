f88124c2984eb65d39af77f533799c569f23439b
komed3 <webmaster@komed3.de>
2026-07-30 11:39:26 +0200

    Create system.py

---
+++ src/rtbnext/endpoint/system.py
@@ -0,0 +1,9 @@
+from rtbnext.endpoint.base import EndpointBase
+
+
+class System( EndpointBase ):
+    """
+    Endpoint implementation for system resources.
+
+    Provides access to system status information.
+    """
