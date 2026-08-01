b8896bd554660268ab54b5ebce6657eaf2c5bd35
komed3 <webmaster@komed3.de>
2026-08-01 10:40:44 +0200

    use regular imports

--- src/rtbnext/core/resource.py
+++ src/rtbnext/core/resource.py
@@ -8,14 +8,10 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from time import time
-from typing import TYPE_CHECKING, Callable, Generic, Literal, TypeVar
+from typing import Any, Callable, Generic, Literal, TypeVar
 
 import httpx
 
 from rtbnext.core.http_client import HttpResponse
 from rtbnext.defaults import DEFAULT_RATE_LIMIT_MODE, DEFAULT_TIMEOUT
-
-if TYPE_CHECKING:
-    from typing import Any
-
-    from rtbnext.core.http_client import HttpClient, HttpHeader, RateLimitMode
+from rtbnext.core.http_client import HttpClient, HttpHeader, RateLimitMode
