eaf6ee659787478248d5ba3eb6097695a91bb79b
komed3 <webmaster@komed3.de>
2026-08-01 11:42:08 +0200

    rename file

--- src/rtbnext/core/cache.py
+++ src/rtbnext/core/cache.py
@@ -12,7 +12,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
 if TYPE_CHECKING:
-    from rtbnext.core.resource import ResourceState
+    from rtbnext.core.resource_loader import ResourceState
 
 
 @runtime_checkable
