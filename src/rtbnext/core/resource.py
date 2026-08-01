9dacf15b9eea9a3c2ee3dc9427f9bbec774f0cf9
komed3 <webmaster@komed3.de>
2026-08-01 10:43:05 +0200

    sort imports

--- src/rtbnext/core/resource.py
+++ src/rtbnext/core/resource.py
@@ -12,9 +12,8 @@ from typing import Any, Callable, Generic, Literal, TypeVar
 
 import httpx
 
-from rtbnext.core.http_client import HttpResponse
+from rtbnext.core.http_client import HttpClient, HttpHeader, HttpResponse, RateLimitMode
 from rtbnext.defaults import DEFAULT_RATE_LIMIT_MODE, DEFAULT_TIMEOUT
-from rtbnext.core.http_client import HttpClient, HttpHeader, RateLimitMode
 
 type CacheType = Literal[ False, "memory" ]
 type CacheMode = Literal[ "ttl", "session", "revalidate" ]
