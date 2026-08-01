f5d4d523b04d19e49fe0817043c1a40d193140dc
komed3 <webmaster@komed3.de>
2026-08-01 16:49:51 +0200

    fix import path

--- src/rtbnext/defaults.py
+++ src/rtbnext/defaults.py
@@ -9,8 +9,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from rtbnext.core.loader import CacheMode, CacheType
     from rtbnext.core.rate_limiter import RateLimitMode
-    from rtbnext.core.resource import CacheMode, CacheType
 
 DEFAULT_API_URL: str = "https://api.rtbnext.de"
 
