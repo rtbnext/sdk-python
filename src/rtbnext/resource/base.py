fd13100191bff7d3180d8f747c04ba881fc872f9
komed3 <webmaster@komed3.de>
2026-08-01 12:08:18 +0200

    refresh the resource from the network

--- src/rtbnext/resource/base.py
+++ src/rtbnext/resource/base.py
@@ -146,6 +146,22 @@ class Resource( Generic[ D ] ):
 
         await self._loading
 
+    async def refresh(
+        self, *,
+        headers: HttpHeader = None,
+        mode: RateLimitMode = DEFAULT_RATE_LIMIT_MODE,
+        timeout: float | None = None
+    ) -> None:
+        """Refresh the resource from the network."""
+
+        self._state = await self._loader.refresh(
+            self._path, headers= headers, mode= mode, timeout= timeout
+        )
+
+        self._loaded = True
+        self._reset()
+        self._emit( "refresh", "update" )
+
 
 class ResourcePool( Generic[ R ] ):
     """
