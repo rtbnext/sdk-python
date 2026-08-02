3dcd35879705b01c6d86419089bed958135b0278
komed3 <webmaster@komed3.de>
2026-08-02 22:57:16 +0200

    prepare types for time series

--- src/rtbnext/resource/series.py
+++ src/rtbnext/resource/series.py
@@ -6,10 +6,16 @@ Implements the resource wrapper for time-series endpoints.
 
 from __future__ import annotations
 
-from typing import TypedDict
+from typing import TypedDict, TypeVar
 
 
 class TimePoint( TypedDict ):
     """Ensure `date` is included in the dict."""
 
     date: str
+
+
+type TimeSeriesRow = list[ str | int | float ]
+
+D = TypeVar( "D", bound= list[ TimeSeriesRow ] )
+R = TypeVar( "R", bound= TimePoint )
