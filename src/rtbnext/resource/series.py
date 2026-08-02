f0711db076fcef8a8cad0606df90c7765758861b
komed3 <webmaster@komed3.de>
2026-08-02 22:56:09 +0200

    declare `TimePoint` to ensure `date` is included in the dict

--- src/rtbnext/resource/series.py
+++ src/rtbnext/resource/series.py
@@ -3,3 +3,13 @@ Time Series Resource
 
 Implements the resource wrapper for time-series endpoints.
 """
+
+from __future__ import annotations
+
+from typing import TypedDict
+
+
+class TimePoint( TypedDict ):
+    """Ensure `date` is included in the dict."""
+
+    date: str
