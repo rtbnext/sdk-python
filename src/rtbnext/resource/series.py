362bed3c6b70f86da9e76ab78d3e5dde4626ab17
komed3 <webmaster@komed3.de>
2026-08-02 23:04:17 +0200

    declare TimeSeriesCollection cls

--- src/rtbnext/resource/series.py
+++ src/rtbnext/resource/series.py
@@ -6,7 +6,9 @@ Implements the resource wrapper for time-series endpoints.
 
 from __future__ import annotations
 
-from typing import TypedDict, TypeVar
+from typing import Generic, TypedDict, TypeVar
+
+from rtbnext.resource.collection import DateCollectionBase
 
 
 class TimePoint( TypedDict ):
@@ -19,3 +21,13 @@ type TimeSeriesRow = list[ str | int | float ]
 
 D = TypeVar( "D", bound= list[ TimeSeriesRow ] )
 R = TypeVar( "R", bound= TimePoint )
+
+
+class TimeSeriesCollection( DateCollectionBase[ R, R ], Generic[ R ] ):
+    """
+    Collection wrapper for time-series resources.
+
+    This class is the base for CSV resource collections, providing methods
+    to access dated data points, aggregate them and get statistical values
+    like min, max, median etc.
+    """
