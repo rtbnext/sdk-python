f4c488ce804191d2ad8a5e80eaf382435a7be7b7
komed3 <webmaster@komed3.de>
2026-08-02 10:31:51 +0200

    returns the deceased profiles filter collection

--- src/rtbnext/endpoint/filter.py
+++ src/rtbnext/endpoint/filter.py
@@ -12,6 +12,8 @@ from rtbnext.endpoint.profile import ProfileEntity
 from rtbnext.resource.collectable import CollectableResource
 from rtbnext.schema.filter import Filter, FilterIndex, FilterItem
 
+type FilterCollection = CollectableResource[ Filter, FilterItem, ProfileEntity[ FilterItem ] ]
+
 
 class FilterEndpoint( EndpointBase ):
     """
@@ -21,9 +23,13 @@ class FilterEndpoint( EndpointBase ):
     demographics, and indices.
     """
 
-    def _filter( self, path: str ) -> CollectableResource[
-        Filter, FilterItem, ProfileEntity[ FilterItem ]
-    ]:
-        """Creates a filter collection using the profile endpoint's collection helper."""
+    def _filter( self, path: str ) -> FilterCollection:
+        """Creates a filter collection using the profile endpoint's collection builder."""
 
         return self._endpoints.profile.collect( path )
+
+    @property
+    def deceased( self ) -> FilterCollection:
+        """Returns the deceased profiles filter collection."""
+
+        return self._filter( "v2/filter/special/deceased.json" )
