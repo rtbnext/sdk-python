7ccd5ed8ce74dc67adea18673228b1b40722b4ce
komed3 <webmaster@komed3.de>
2026-08-03 17:58:12 +0200

    implement filter examples

---
+++ examples/filter.py
@@ -0,0 +1,39 @@
+import asyncio
+from rtbnext import ClientIdentity, rtbnext
+
+
+async def main() -> None:
+    async with rtbnext( client= ClientIdentity(
+        name= "python-sdk-test",
+        version= "1.0.0",
+        contact="https://pypi.org/project/rtbnext"
+    ) ) as client:
+
+        # filter profiles
+        female = await client.filter.gender( "f" ).collection()
+        print( "Female profiles:", female.count )
+
+        for item in female.take( 10 ):
+            print( item["name"], item["uri"] )
+
+        # use filter index ---
+
+        index = await client.filter.index.get()
+        us = await index["country"]["US"].collection()
+        print( "U.S. profiles:", us.count )
+
+        for item in us.take( 10 ):
+            print( item["name"], item["uri"] )
+
+        # complex filter queries
+        tech = await client.filter.industry( "technology" ).collection()
+        age_40_49 = await client.filter.age( "40" ).collection()
+        profiles = female.intersect( tech ).intersect( age_40_49 )
+
+        print( "Matching profiles:", profiles.count )
+
+        for item in profiles:
+            print( item["name"], item["uri"] )
+
+
+asyncio.run( main() )
