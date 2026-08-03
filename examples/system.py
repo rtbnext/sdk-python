f84ed19c1f5effff297a8a33dbc75d430dbe6acf
komed3 <webmaster@komed3.de>
2026-08-03 15:30:15 +0200

    skip / exclude folders from lint/isort

---
+++ examples/system.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from rtbnext import ClientIdentity, rtbnext
+
+
+async def main() -> None:
+    async with rtbnext( client= ClientIdentity(
+        name= "python-sdk-test",
+        version= "1.0.0"
+    ) ) as client:
+
+        # get current system status
+        status = await client.system.status.data()
+        print( "System status:", status["status"] )
+        print( "Profile status:", status["services"]["profile"] )
+
+
+asyncio.run( main() )
