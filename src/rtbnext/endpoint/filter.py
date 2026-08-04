ed639fc6d44d7faa707b3d48a71a3c31e1385d49
komed3 <webmaster@komed3.de>
2026-08-04 13:58:27 +0200

    Update filter.py

--- src/rtbnext/endpoint/filter.py
+++ src/rtbnext/endpoint/filter.py
@@ -17,17 +17,18 @@ from rtbnext.schema.filter import Filter, FilterIndex, FilterItem
 from rtbnext.schema.generic import AgeGroup, Gender, Industry, MaritalStatus
 from rtbnext.utils import sanitize
 
-type FilterCollection = CollectableResource[ Filter, FilterItem, ProfileEntity[ FilterItem ] ]
+type FilterResource = CollectableResource[ Filter, FilterItem, ProfileEntity[ FilterItem ] ]
+type FilterIndexResource = IndexableResource[ FilterIndex, FilterIndexTree ]
 
 FilterIndexTree = TypedDict( "FilterIndexTree", {
-    "industry": dict[ Industry, FilterCollection ],
-    "citizenship": dict[ str, FilterCollection ],
-    "country": dict[ str, FilterCollection ],
-    "state": dict[ str, FilterCollection ],
-    "gender": dict[ Gender, FilterCollection ],
-    "age": dict[ AgeGroup, FilterCollection ],
-    "maritalStatus": dict[ MaritalStatus, FilterCollection ],
-    "special": dict[ str, FilterCollection ]
+    "industry": dict[ Industry, FilterResource ],
+    "citizenship": dict[ str, FilterResource ],
+    "country": dict[ str, FilterResource ],
+    "state": dict[ str, FilterResource ],
+    "gender": dict[ Gender, FilterResource ],
+    "age": dict[ AgeGroup, FilterResource ],
+    "maritalStatus": dict[ MaritalStatus, FilterResource ],
+    "special": dict[ str, FilterResource ]
 } )
 
 
@@ -39,72 +40,72 @@ class FilterEndpoint( EndpointBase ):
     demographics, and indices.
     """
 
-    def _filter( self, path: str ) -> FilterCollection:
+    def _filter( self, path: str ) -> FilterResource:
         """Creates a filter collection using the profile endpoint's collection builder."""
 
         return self._endpoints.profile.collect( path )
 
     @property
-    def deceased( self ) -> FilterCollection:
+    def deceased( self ) -> FilterResource:
         """Returns the filter collection for deceased profiles."""
 
         return self._filter( "v2/filter/special/deceased.json" )
 
     @property
-    def drop_off( self ) -> FilterCollection:
+    def drop_off( self ) -> FilterResource:
         """Returns the filter collection for dropped off profiles."""
 
         return self._filter( "v2/filter/special/dropOff.json" )
 
     @property
-    def family( self ) -> FilterCollection:
+    def family( self ) -> FilterResource:
         """Returns the filter collection for family profiles."""
 
         return self._filter( "v2/filter/special/family.json" )
 
     @property
-    def self_made( self ) -> FilterCollection:
+    def self_made( self ) -> FilterResource:
         """Returns the filter collection for self-made profiles."""
 
         return self._filter( "v2/filter/special/selfMade.json" )
 
-    def industry( self, key: Industry ) -> FilterCollection:
+    def industry( self, key: Industry ) -> FilterResource:
         """Returns the filter collection by industry."""
 
         return self._filter( f"v2/filter/industry/{ sanitize( key ) }.json" )
 
-    def age( self, key: AgeGroup ) -> FilterCollection:
+    def age( self, key: AgeGroup ) -> FilterResource:
         """Returns the filter collection by age group."""
 
         return self._filter( f"v2/filter/age/{ key }.json" )
 
-    def gender( self, key: Gender ) -> FilterCollection:
+    def gender( self, key: Gender ) -> FilterResource:
         """Returns the filter collection by gender."""
 
         return self._filter( f"v2/filter/gender/{ sanitize( key ) }.json" )
 
-    def marital_status( self, key: MaritalStatus ) -> FilterCollection:
+    def marital_status( self, key: MaritalStatus ) -> FilterResource:
         """Returns the filter collection by marital status."""
 
         return self._filter( f"v2/filter/maritalStatus/{ sanitize( key ) }.json" )
 
-    def citizenship( self, key: str ) -> FilterCollection:
+    def citizenship( self, key: str ) -> FilterResource:
         """Returns the filter collection by citizenship, indexed by ISO code."""
 
         return self._filter( f"v2/filter/citizenship/{ key.upper() }.json" )
 
-    def country( self, key: str ) -> FilterCollection:
+    def country( self, key: str ) -> FilterResource:
         """Returns the filter collection by country, indexed by ISO code."""
 
         return self._filter( f"v2/filter/country/{ key.upper() }.json" )
 
-    def state( self, key: str ) -> FilterCollection:
+    def state( self, key: str ) -> FilterResource:
         """Returns the filter collection by U.S. state, indexed by USPS code."""
 
         return self._filter( f"v2/filter/state/{ key.upper() }.json" )
 
     @property
-    def index( self ) -> IndexableResource[ FilterIndex, FilterIndexTree ]:
+    def index( self ) -> FilterIndexResource:
         """Provides the root filter index resource."""
 
         return self._indexable( "v2/filter/index.json",
