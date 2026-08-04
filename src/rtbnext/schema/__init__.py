281d66eafcf248f15d56fc585f0bc7c32230a957
komed3 <webmaster@komed3.de>
2026-08-04 12:42:52 +0200

    provide public type definitions for RTBNext API responses

---
+++ src/rtbnext/schema/__init__.py
@@ -0,0 +1,21 @@
+"""
+RTBNext Schema
+
+Provides public type definitions for RTBNext API responses.
+
+Exports TypedDicts, literals, and type aliases describing the structure of
+JSON responses returned by API endpoints. These definitions can be used for
+static type checking, editor support, and custom integrations.
+
+Author: Paul Köhler (komed3)
+License: MIT
+"""
+
+from rtbnext.schema.assets import *
+from rtbnext.schema.filter import *
+from rtbnext.schema.generic import *
+from rtbnext.schema.list import *
+from rtbnext.schema.mover import *
+from rtbnext.schema.profile import *
+from rtbnext.schema.stats import *
+from rtbnext.schema.system import *
