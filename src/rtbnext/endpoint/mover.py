73f42ac426b670a4039203d1007dfcc24e8bf8fc
komed3 <webmaster@komed3.de>
2026-08-04 13:56:56 +0200

    declare mover endpoint type aliases

--- src/rtbnext/endpoint/mover.py
+++ src/rtbnext/endpoint/mover.py
@@ -13,6 +13,9 @@ from rtbnext.schema.generic import DateIndex
 from rtbnext.schema.mover import Mover
 from rtbnext.utils import ymd
 
+type MoverSnapshotResource = Resource[ Mover ]
+type MoverIndexResource = DateableResource[ DateIndex, Resource[ Mover ] ]
+
 
 class MoverEndpoint( EndpointBase ):
     """
@@ -21,13 +24,13 @@ class MoverEndpoint( EndpointBase ):
     Provides access to mover snapshots and mover index resources.
     """
 
-    def snapshot( self, date: object ) -> Resource[ Mover ]:
+    def snapshot( self, date: object ) -> MoverSnapshotResource:
         """Returns a mover snapshot for a given date."""
 
         return self._resource( f"v2/mover/{ ymd( date ) }.json" )
 
     @property
-    def index( self ) -> DateableResource[ DateIndex, Resource[ Mover ] ]:
+    def index( self ) -> MoverIndexResource:
         """Returns the root mover index resource."""
 
         return self._dateable( "v2/mover/index.json", date= self.snapshot )
