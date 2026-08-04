fefcddeaf7d46d2f2803a800a920edd8b020660d
komed3 <webmaster@komed3.de>
2026-08-04 14:24:38 +0200

    Create `__init__.py` for endpoints

---
+++ src/rtbnext/endpoint/__init__.py
@@ -0,0 +1,19 @@
+"""
+RTBNext Endpoint Schema
+
+Provides public endpoint type definitions for RTBNext resources.
+
+Exports typed resource aliases describing the return types of API endpoints.
+These definitions combine resources, collections, entities, and specialized
+data structures to provide precise type information for SDK integrations.
+
+Author: Paul Köhler (komed3)
+License: MIT
+"""
+
+from rtbnext.endpoint.filter import *
+from rtbnext.endpoint.list import *
+from rtbnext.endpoint.mover import *
+from rtbnext.endpoint.profile import *
+from rtbnext.endpoint.stats import *
+from rtbnext.endpoint.system import *
